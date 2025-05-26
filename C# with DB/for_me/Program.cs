801633daa924ab6e54632e1427a659527499bba6
your name <you@example.com>
2025-05-26 22:06:00 +0400

    basic c# project with ssms21 DataBase

---
+++ C# with DB/for_me/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Data.SqlClient;
+var connectionstring = @"Server=(localdb)\mssqllocaldb;Database=test;Trusted_Connection=True;";
+
+SqlConnection connection = new SqlConnection(connectionstring);
+connection.Open();
+
+var insert = "INSERT INTO dbo.person VALUES ('4','ri','li')";
+SqlCommand command = new SqlCommand(insert, connection); 
+
+command.ExecuteNonQuery();
+Console.WriteLine("done!" );
+
+connection.Close();
